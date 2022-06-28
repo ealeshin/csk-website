4ca9df8019aace8a854e37cbe0e598e1f2b8b297
Evgeny Aleshin <evgenyaleshin@icloud.com>
2022-06-28 22:42:20 +0300

    Rework navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Центр Строительной Комплектации</title>
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="packages/line-awesome/css/line-awesome.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
     <script defer src="js/app.js"></script>
@@ -13,21 +14,40 @@
 <body>
     <header>
         <div class="column">
-            <div class="column">logo</div>
-            <div class="column">phone</div>
-            <div class="column">search</div>
+            <div class="column">
+                <img src="images/logo.svg" alt="CSK Logo" class="logo">
+            </div>
+            <div class="column nav">
+                <div class="row search-container">
+                    Поиск
+                </div>
+                <div class="row">
+                    <div class="column">
+                        <a href="#" class="nav-link">О компании</a>
+                    </div>
+                    <div class="column">
+                        <a href="#" class="nav-link">Каталог ЦСК</a>
+                    </div>
+                    <div class="column">
+                        <a href="#" class="nav-link">Контакты</a>
+                    </div>
+                    <div class="column">
+                        <a href="#" class="nav-link">Наши партнёры</a>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="column">
-            <div class="column">question</div>
-            <div class="column">cart</div>
+            <div class="column">
+                <i class="la la-question icon"></i>
+                Вопрос директору
+            </div>
+            <div class="column">
+                <i class="la la-shopping-cart icon"></i>
+                Корзина [0]
+            </div>
         </div>
     </header>
-    <nav>
-        <a href="#">О компании</a>
-        <a href="#">Каталог ЦСК</a>
-        <a href="#">Контакты</a>
-        <a href="#">Наши партнёры</a>
-    </nav>
     <div class="swiper">
         <div class="swiper-wrapper">
             @foreach($slides as $url)
