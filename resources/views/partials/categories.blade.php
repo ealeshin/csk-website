7a577d221001399ee165d71b300024643307855a
Evgeny Aleshin <evgenyaleshin@icloud.com>
2022-07-01 16:07:02 +0300

    Move categories layout to blade partials

---
+++ resources/views/partials/categories.blade.php
@@ -0,0 +1,39 @@
+<section id="categories">
+    <div class="items">
+        <div class="category">
+            <a href="#" class="root-category">Лакокрасочные материалы</a><br>
+            <a href="#">Акриловые составы</a><br>
+            <a href="#">Декоративные покрытия</a><br>
+            <a href="#">Пены монтажные и герметики</a><br>
+        </div>
+        <div class="category">
+            <a href="#" class="root-category">Инструменты</a><br>
+            <a href="#">Штукатурно-малярный инструмент</a><br>
+            <a href="#">Ленты клеящие, малярные</a><br>
+            <a href="#">Измерительный инструмент</a><br>
+        </div>
+        <div class="category">
+            <a href="#" class="root-category">Категория</a><br>
+            <a href="#">Подкатегория первая</a><br>
+            <a href="#">Подкатегория вторая</a><br>
+        </div>
+        <div class="category">
+            <a href="#" class="root-category">Категория</a><br>
+            <a href="#">Подкатегория первая</a><br>
+            <a href="#">Подкатегория вторая</a><br>
+        </div>
+        <div class="category">
+            <a href="#" class="root-category">Категория</a><br>
+            <a href="#">Подкатегория первая</a><br>
+            <a href="#">Подкатегория вторая</a><br>
+            <a href="#">Подкатегория третья</a><br>
+            <a href="#">Подкатегория четвёртая</a><br>
+        </div>
+        <div class="category">
+            <a href="#" class="root-category">Категория</a><br>
+            <a href="#">Подкатегория первая</a><br>
+            <a href="#">Подкатегория вторая</a><br>
+            <a href="#">Подкатегория третья</a><br>
+        </div>
+    </div>
+</section>
