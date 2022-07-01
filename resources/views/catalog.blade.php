00ffcd5ea507da693a5f575b599dd7bb785d645e
Evgeny Aleshin <evgenyaleshin@icloud.com>
2022-07-01 22:05:10 +0300

    Catalog sidebar wip

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,12 +1,17 @@
 @include('partials.header')
     <section id="categories">
-        <div class="items">
-            @foreach($products as $product)
-            <div class="card">
-                <div class="card-image" style="background-image: url('{{$product->images[0]['image']}}');"></div>
-                <div class="card-title">{{$product->name}}</div>
-                <a class="card-more" href="product/{{$product->id}}">Подробнее о товаре</a>
+        <div class="catalog">
+            <div class="sidebar">
+                sidebar<br>content
             </div>
-            @endforeach
+            <div class="items">
+                @foreach($products as $product)
+                <div class="card">
+                    <div class="card-image" style="background-image: url('{{$product->images[0]['image']}}');"></div>
+                    <div class="card-title">{{$product->name}}</div>
+                    <a class="card-more" href="product/{{$product->id}}">Подробнее о товаре</a>
+                </div>
+                @endforeach
+            </div>
         </div>
     </section>
