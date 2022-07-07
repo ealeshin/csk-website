24d07f228d141bd8a93fcb9a9b75232e31a81ca4
Evgeny Aleshin <evgenyaleshin@icloud.com>
2022-07-07 22:49:26 +0300

    Add cart data getter to cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -16,6 +16,34 @@ class CartController extends Controller
         $this->update('delete', $request);
     }
 
+    public static function getCartCounter(Request $request)
+    {
+        $count = 0;
+        $data = $request->session()->all();
+        foreach ($data as $key => $value) {
+            if (str_contains($key, 'product_')) {
+                $count++;
+            }
+        }
+        return $count;
+    }
+
+    public function get(Request $request)
+    {
+        $data = $request->session()->all();
+        $cart = [];
+        foreach ($data as $key => $value) {
+            if (str_contains($key, 'product_')) {
+                $id = substr($key, strpos($key, '_') + 1);
+                $cart[$id] = $value;
+            }
+        }
+        return response()->json([
+            'success' => true,
+            'data' => $cart
+        ], 200);
+    }
+
     public function dump(Request $request)
     {
         $data = $request->session()->all();
