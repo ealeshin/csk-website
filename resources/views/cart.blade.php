0f2587e32c02deb76dc7d17cbc5b3cb892fcb065
Evgeny Aleshin <evgenyaleshin@icloud.com>
2022-07-08 17:02:29 +0300

    Pass data to cart blade

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,11 +1,41 @@
 @include('partials.header')
-    <section id="mp-catalog">
-        <div class="items">
+    <section id="cart">
+        <div class="cart">
             @foreach($items as $item)
-            <div class="card">
-                <div class="card-image" style="background-image: url('{{$item['product']->images[0]['image']}}');"></div>
-                <div class="card-title">{{$item['product']->name}}</div>
-                <div>Количество: {{$item['count']}}</div>
+            <div class="cart-item" data-cart-id="{{$item['product']->id}}">
+                <div class="cart-image" style="background-image: url('{{$item['product']->images[0]['image']}}');">
+                </div>
+                <div class="cart-product">
+                    <div class="cart-block-title">Товар</div>
+                    <div class="cart-block-value product-name">
+                        <a href="/product/{{$item['product']->id}}">{{$item['product']->name}}</a>
+                    </div>
+                </div>
+                <div class="cart-block-column">
+                    <div class="cart-block-title">Цена</div>
+                    <div class="cart-block-value">
+                        {{number_format($item['product']->price, 2, ',', ' ')}}
+                    </div>
+                </div>
+                <div class="cart-block-column">
+                    <div class="cart-block-title">Количество</div>
+                    <div class="cart-block-value">{{$item['count']}}</div>
+                </div>
+                <div class="cart-block-column">
+                    <div class="cart-block-title">Сумма</div>
+                    <div class="cart-block-value">
+                        {{number_format($item['count'] * $item['product']->price, 2, ',', ' ')}}
+                    </div>
+                </div>
+                <div class="cart-block-column">
+                    <div class="cart-block-title">&nbsp;</div>
+                    <div class="cart-block-value">
+                        <a href="javascript:void(0);" data-id="{{$item['product']->id}}"
+                                class="delete-from-cart" title="Убрать из корзины">
+                            <i class="la la-trash-alt nav-icon"></i>
+                        </a>
+                    </div>
+                </div>
             </div>
             @endforeach
         </div>
