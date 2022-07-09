838e3b73c921bf1617afa3a22efcfd50b64bf0ae
Evgeny Aleshin <evgenyaleshin@icloud.com>
2022-07-09 19:53:53 +0300

    Add modal blade

---
+++ resources/views/modal.blade.php
@@ -0,0 +1,14 @@
+<div class="shield">
+    <div class="modal modal-form">
+        <div class="modal-block">Введите ваше имя и контактные данные</div>
+        <input type="text" placeholder="Имя">
+        <input type="text" placeholder="Телефон">
+        <input type="text" placeholder="E-mail">
+        <div class="modal-block">
+            <a href="javascript:void(0);" class="button">Отправить</a>
+        </div>
+    </div>
+    <div class="modal modal-success">
+        <div class="modal-block">Ваш заказ принят!</div>
+    </div>
+</div>
