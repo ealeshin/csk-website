ebdd607a7fe8b9f2d54f33418e47dc9a97c224b1
Evgeny Aleshin <evgenyaleshin@icloud.com>
2022-07-11 15:27:36 +0300

    Add search result message

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,18 +1,22 @@
 @include('partials.header')
-@if($count > 0)
-<h2>Найдено товаров: {{$count}}</h2>
 <section id="mp-catalog">    
     <div class="items">
-        @foreach($products as $product)
-        <div class="card">
-            <div class="card-image" style="background-image: url('{{$product->images[0]['image']}}');"></div>
-            <div class="card-title">{{$product->name}}</div>
-            <a class="card-more" href="/product/{{$product->id}}">Подробнее о товаре</a>
+        <div class="items-heading">
+            @if($count > 0)
+                <h3>Найдено товаров: {{$count}}</h3>
+            @else
+                <h3>По вашему запросу ничего не найдено</h3>
+            @endif
         </div>
-        @endforeach
+        @if($count > 0)
+            @foreach($products as $product)
+            <div class="card">
+                <div class="card-image" style="background-image: url('{{$product->images[0]['image']}}');"></div>
+                <div class="card-title">{{$product->name}}</div>
+                <a class="card-more" href="/product/{{$product->id}}">Подробнее о товаре</a>
+            </div>
+            @endforeach
+        @endif
     </div>
 </section>
-@else
-<h2>По вашему запросу ничего не найдено</h2>
-@endif
 @include('partials.footer')
