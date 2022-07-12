ae3faa37e98e9c65d3b27a748ed30405b61e6eb6
Evgeny Aleshin <evgenyaleshin@icloud.com>
2022-07-12 23:43:51 +0300

    Add mail sending to form handler

--- app/Services/FormHandler.php
+++ app/Services/FormHandler.php
@@ -7,13 +7,55 @@ class FormHandler
     protected $name;
     protected $phone;
     protected $email;
+    protected $text;
     protected $data = [];
+    protected $to;
 
-    public function __construct($name, $phone, $email, $data = [])
+    public function __construct($name, $phone, $email, $text = '', $data = [])
     {
         $this->name = $name;
         $this->phone = $phone;
         $this->email = $email;
+        $this->text = $text;
         $this->data = $data;
+        $this->to = env('MAIL_TO');
+    }
+
+    public function sendOrder()
+    {
+        $data = $this->getCommonFields();
+        $data +=  ['data' => $this->data];
+        $view = view('mail.order', $data)->render();
+
+        return $this->sendEmail($this->to, 'Order', $view);
+    }
+
+    public function sendQuestion()
+    {
+        $data = $this->getCommonFields();
+        $data +=  ['question' => $this->text];
+        $view = view('mail.question', $data)->render();
+
+        return $this->sendEmail($this->to, 'Question', $view);
+    }
+
+    protected function getCommonFields()
+    {
+        return [
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'email' => $this->email
+        ];
+    }
+
+    protected function sendEmail($to, $subject, $message)
+    {
+        $headers = "From: mail@example.com\r\n"
+            ."Content-type: text/html; charset=utf-8\r\n"
+            ."X-Mailer: PHP mail script";
+
+        $result = mail($to, $subject, $message, $headers);
+
+        return $result;
     }
 }
